c816c985996ae61cb1a77aa549c7a4f49d36dbee
Gleb Klimov <gleb.klimov@fortegrp.com>
2023-05-18 22:12:23 +0200

    SOE-3 Modify Footer

--- packages/graphiql/src/components/GraphiQL.tsx
+++ packages/graphiql/src/components/GraphiQL.tsx
@@ -710,20 +710,25 @@ export function GraphiQLInterface(props: GraphiQLInterfaceProps) {
         </div>
       </div>
       <div className="graphiql-footer">
-        <span className="footer-policy">
-          © 2023 SuiQL This site is protected by reCAPTCHA<br/>
-          and the Google <a href="https://policies.google.com/privacy" target="_blank">Privacy Policy </a>
-          and <a href="https://policies.google.com/terms" target="_blank">Terms of Service</a> apply.
-        </span>
-        <div className="footer-madeBy">
-          <span>Made with &nbsp;</span>
-          <img src={heartImg} alt="heartImg"/>
-          <span>&nbsp;by&nbsp;</span>
-          <a href="https://peeranha.io" target="_blank"><img src={peeranhaImg} alt="peeranhaImg"/></a>
+        <div className="graphiql-footer-left">
+          <button className="footer-aboutUs">About us</button>
+          <span className="footer-policy">
+            © 2023 SuiQL This site is protected by reCAPTCHA
+            and the Google <a href="https://policies.google.com/privacy" target="_blank">Privacy Policy </a>
+            and <a href="https://policies.google.com/terms" target="_blank">Terms of Service</a> apply.
+          </span>
         </div>
-        <div className="footer-socials">
-          <a href="https://discord.gg/dQCkxUN74W" target="_blank"><img src={discordImg} alt="discordImg"/></a>
-          <a href="https://twitter.com/Sui_QL" target="_blank"><img src={twitterImg} alt="twitterImg"/></a>
+        <div className="graphiql-footer-right">
+          <div className="footer-socials">
+            <a href="https://discord.gg/dQCkxUN74W" target="_blank"><img src={discordImg} alt="discordImg"/></a>
+            <a href="https://twitter.com/Sui_QL" target="_blank"><img src={twitterImg} alt="twitterImg"/></a>
+          </div>
+          <div className="footer-madeBy">
+            <span>Made with &nbsp;</span>
+            <img src={heartImg} alt="heartImg"/>
+            <span>&nbsp;by&nbsp;</span>
+            <a href="https://peeranha.io" target="_blank"><img src={peeranhaImg} alt="peeranhaImg"/></a>
+          </div>
         </div>
       </div>
       <Dialog
